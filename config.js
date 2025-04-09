cb990070fb6d81791f5a8f19dad8f80d162be622
Coconuteagle <131874183+Coconuteagle@users.noreply.github.com>
2025-04-09 13:27:56 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,27 @@
+// config.js
+window.apiKeys = {
+  GOOGLE_SEARCH_CONFIGS: [
+    {
+      googleApiKey: "AIzaSyDndNAfYE6-_tpcQk5mRlbNGduvFN_WF2o",
+      searchEngineId: "02aca10304c1d4564"
+    },
+    {
+      googleApiKey: "AIzaSyBXoERTUBALRJl6k2OsJ5tGnElcSZMsBfA",
+      searchEngineId: "552ad4c6d70ca433e"
+    },
+    {
+      googleApiKey: "AIzaSyDL01M_fwCJGve-We30Lx271mHpruJofpM",
+      searchEngineId: "c740225da5cb9464d"
+    },
+    {
+      googleApiKey: "AIzaSyD2MdhssL0ld1jcOk5CIA9UdAEyVUlL0Is",
+      searchEngineId: "c490e860d8a5a414a"
+    },
+    {
+      googleApiKey: "AIzaSyAhVhb4mNKre2kS-XAxtU7M-yAwCWN-Wkw",
+      searchEngineId: "46b83232b96ef4fa4"
+    },
+    // ...
+  ],
+  GEMINI_API_KEY: "AIzaSyCptpJ68R5lyJPduY8rtqUXR9Ij7F4puoE"
+};
